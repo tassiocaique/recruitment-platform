ea2dda7dcda8abd7f6371b5452461864b981bf98
victorgso001 <victor.gso001@gmail.com>
2019-04-15 14:53:14 -0300

    Criação inicial dos cards

---
+++ resources/assets/js/features/curriculas/pages/sentCurricula.vue
@@ -0,0 +1,26 @@
+<template>
+        <div class = "cards">
+                <v-layout row wrap>
+                    <v-flex xs12 v-for="card in cards" :key="card.name">
+                        <v-card class="mb-2">hello</v-card>
+                    </v-flex>
+                </v-layout>    
+        </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            cards: {name: 'Jorge Vieira da Silva',
+            role: 'Estágio',
+            submissionDate: '14/01/2019',
+            phone: '(87) 98812-1212',
+            email: 'jorgesilva@hotmail.com',
+            github: '/jorgevcod',
+            linkedin: '/jorgevthecoder',
+            tags: ['front-end', 'back-end', 'designer']}
+        }
+    }
+}
+</script>
