21fe10b88b0eee1235f0d70b5b68fc8ccde21bf3
victorgso001 <victor.gso001@gmail.com>
2019-04-15 14:52:47 -0300

    Modificações para nova rota do component sentCurricula

--- resources/assets/js/core/routes.js
+++ resources/assets/js/core/routes.js
@@ -4,15 +4,22 @@ import VueRouter from 'vue-router'
 Vue.use(VueRouter)
 
 import Curriculum from '../features/curriculas/components/CurriculumComponent'
+import SentCurricula from '../features/curriculas/pages/sentCurricula'
 
 const router = new VueRouter({
     mode: 'history',
     routes: [
         {
             path: '/curriculum',
+            children: [
+                {
+                    path: 'sentCurricula',
+                    component: SentCurricula,
+                }
+            ],
             component: Curriculum
         }
     ]
-})
+});
 
 export default router;
