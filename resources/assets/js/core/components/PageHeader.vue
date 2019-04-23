2bf839cc57ec0691e4efe100c7da3ac68a4e613a
Joao Paulo <joaopaulosncastro@gmail.com>
2019-04-23 11:03:43 -0300

    Adequa o código ao padrão airbnb com o ESLint

--- resources/assets/js/core/components/PageHeader.vue
+++ resources/assets/js/core/components/PageHeader.vue
@@ -1,36 +1,40 @@
 <template>
 	<v-toolbar
-	color="primary"
-	tabs
+		color="primary"
+		tabs
 	>
-		<img src="https://sysvale.com/img/sysvale-white.svg" width="146">
-	
-		<v-spacer></v-spacer>
+		<img
+			src="https://sysvale.com/img/sysvale-white.svg"
+			width="146"
+		>
+		<v-spacer />
 
-		<v-toolbar-title class="white--text">{{ name }}</v-toolbar-title>
+		<v-toolbar-title class="white--text">
+			{{ name }}
+		</v-toolbar-title>
 
 		<v-btn icon>
-			<v-icon color="white">logout</v-icon>
+			<v-icon color="white">
+				logout
+			</v-icon>
 		</v-btn>
 
 		<template v-slot:extension>
-				<v-tabs
-					v-model="model"
-					color="primary"
-					slider-color="sliderColor"
+			<v-tabs
+				v-model="model"
+				color="primary"
+				slider-color="sliderColor"
+			>
+				<v-tab
+					v-for="(i, index) in items"
+					:key="index"
+					class="white--text"
+					:href="`#tab-${i.value}`"
 				>
-					<v-tab
-						class="white--text"
-						v-for="(i, index) in items"
-						:key="index"
-						:href="`#tab-${i.value}`"
-					>
 					{{ i.value }}
-					</v-tab>
-
-				</v-tabs>
-			</template>
-
+				</v-tab>
+			</v-tabs>
+		</template>
 	</v-toolbar>
 </template>
 
@@ -38,21 +42,21 @@
 export default {
 	data() {
 		return {
-			items: [{
-				value: 'HOME'
-			},
-			{
-				value: 'AGENDA'
-			},
-			{
-				value: 'ARQUIVADOS'
-			},
+			items: [
+				{
+					value: 'HOME',
+				},
+				{
+					value: 'AGENDA',
+				},
+				{
+					value: 'ARQUIVADOS',
+				},
 			],
 
 			name: 'Tassio Caique',
 		};
-	}
-	
-}
-</script>
+	},
 
+};
+</script>
